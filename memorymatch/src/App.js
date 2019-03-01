2a3e22ce8c53780f395e23f5bfd83a724b804e98
Martin92571 <zegrizo@gmail.com>
2019-02-28 21:10:01 -0800

    added the PlayerStart.js

--- memorymatch/src/App.js
+++ memorymatch/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PlayerData from './PlayerSection';
+import MemoryCards from './MemoryCards';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
          <PlayerData/>
+         <MemoryCards/>
       </div>
     );
   }
--- memorymatch/src/MemoryCards.js
+++ memorymatch/src/MemoryCards.js
@@ -1,1 +1,10 @@
 import React from 'react';
+const CardBoard=(props)=>{
+    return(
+        <div class="gameArea ">
+       
+        </div>
+    )
+}
+
+export default CardBoard
